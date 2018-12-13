a7e7f9110bee4e930b18b5310ea9c5c193d72ea9
宝力 <liubl@8space.com.cn>
2018-12-13 17:11:07 +0800

    无关提交

---
+++ src/main/java/com/lboeri/boeriapi/BoeriApiApplication.java
@@ -0,0 +1,12 @@
+package com.lboeri.boeriapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoeriApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoeriApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lboeri/boeriapi/BoeriApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lboeri.boeriapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoeriApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
