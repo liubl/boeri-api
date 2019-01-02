4d0eb7c3e53188c9736e6c9e700f861b4fd158d7
liubl <290223793@qq.com>
2019-01-02 23:41:39 +0800

    多数据源进行中
    进行中

--- src/main/java/com/lboeri/boeriapi/dao/MybatisConfig.java
+++ src/main/java/com/lboeri/boeriapi/dao/MybatisConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MybatisConfig {
@@ -17,9 +19,10 @@ public class MybatisConfig {
     @Autowired
     private DataSourceProperties dataSourceProperties;
 
+
+
     @Bean(name = "dataSource")
     public DataSource dataSource() {
-
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(dataSourceProperties.getUrl());
         System.out.println(dataSourceProperties.getUrl());
@@ -30,6 +33,21 @@ public class MybatisConfig {
         return dataSource;
 
     }
+
+//    @Bean(name = "dataSource")
+//    public DataSourceRouter dataSource() {
+//        DataSourceRouter dataSourceRouter = new DataSourceRouter();
+//        Map<Object, Object> targetDataSources = new HashMap<Object, Object>();
+//        DruidDataSource dataSource = new DruidDataSource();
+//        dataSource.setUrl(dataSourceProperties.getUrl());
+//        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+//        dataSource.setUsername(dataSourceProperties.getUsername());
+//        dataSource.setPassword(dataSourceProperties.getPassword());
+//        targetDataSources.put("defualt", dataSource);
+//        dataSourceRouter.setTargetDataSources(targetDataSources);
+//        return dataSourceRouter;
+//
+//    }
 
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
